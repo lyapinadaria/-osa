013b22ea0ec066cd4ba06d47698253be66bb32cd
lyapinadaria <73475798+lyapinadaria@users.noreply.github.com>
2021-03-12 02:21:27 +0300

    Update cosac.py

--- cosac.py
+++ cosac.py
@@ -8,11 +8,12 @@ from  Cosa.WhiteDwarf import *
 from  Cosa import test
 
 def main():
-   
+   %Запуск тестов
    a=test.TestFunc()
    a.test_get_Nh()
    a.test_get_Nh2()
    a.test_aprox()
+   %Запуск программы
    glamurny_carlik()
 
 if __name__ == '__main__':
