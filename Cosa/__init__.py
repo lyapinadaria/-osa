ab680a9fd0402e53f4cc464de920dfd437be4256
lyapinadaria <73475798+lyapinadaria@users.noreply.github.com>
2021-03-12 01:55:29 +0300

    Add files via upload

---
+++ Cosa/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 23:29:40 2021
+
+@author: User
+"""
+from .WhiteDwarf import *
+from .WhiteDwarf import glamurny_carlik
+print('\nNIT_OK')
+print(WhiteDwarf.glamurny_carlik())
